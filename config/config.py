f41dd58f33c400b07df4a4e49b7d92d077406095
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2024-06-20 17:11:01 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001583255537"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME" "LOVER MUSIC")
 
 # Your User ID.
 OWNER_ID = list(
